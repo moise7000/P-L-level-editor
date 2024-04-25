8198f000fb9252a126de278afadcbd97660b2abc
moise7000 <ewan.decima@yahoo.com>
2024-04-25 20:59:27 +0200

    [+] Main View

--- P-L-Level-Editor/P_L_Level_EditorApp.swift
+++ P-L-Level-Editor/P_L_Level_EditorApp.swift
@@ -8,3 +8,26 @@
 import SwiftUI
 import SwiftData
 
+@main
+struct levelEditorApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            SavedLevelsModel.self, AssetsDataModel.self
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
